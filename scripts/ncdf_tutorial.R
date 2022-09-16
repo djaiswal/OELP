fe4ec4cd55b8dde51dce83213922a0d1a0ed6a2b
Deepak Jaiswal <deepakuiuc@gmail.com>
2022-09-16 15:00:54 +0530

    file name is changed

--- scripts/ncdf_tutorial.R
+++ scripts/ncdf_tutorial.R
@@ -1,4 +1,4 @@
 library(ncdf4)
-myfile <- "/Users/deepakjaiswal/Desktop/tmp/sfcWindAdjust_bced_1960_1999_ipsl-cm5a-lr_rcp4p5_2021-2030.nc"
-
+myfile <- "/Users/deepakjaiswal/Desktop/tmp/rhum.2m.1994.nc"
 climdatedata <- nc_open(filename = myfile)
+print(climdatedata)
